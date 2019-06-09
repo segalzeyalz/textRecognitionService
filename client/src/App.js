62fd1907c5c1f0178c82b73600f6f285b5562111
segalzeyalz <segaleseo@gmail.com>
2019-06-09 10:13:20 +0300

    save wrapper

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,38 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import Typohraphy from "@material-ui/core//Typography";
+import Paper from "@material-ui/core/Paper";
+import PropTypes from "prop-types";
 
-class App extends Component {
-  render() {
-    return <div>I'M READY TO USE THE BACK END APIS! :-)</div>;
+import { createMuiTheme } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/styles";
+
+const theme = createMuiTheme();
+
+const styles = theme => ({
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start"
+  },
+  actions: {
+    display: "flex",
+    justifyContent: "flex-end",
+    backgroundColor: theme.palette.primary.dark,
+    position: "sticky",
+    top: 0
   }
-}
+});
 
-export default App;
+const App = ({ classes }) => {
+  return (
+    <Paper className="App" className={classes.wrapper}>
+      <Typohraphy variant="h1">Input</Typohraphy>
+    </Paper>
+  );
+};
+
+App.propTypes = {
+  classes: PropTypes.object
+};
+
+export default withStyles(styles(theme))(App);
