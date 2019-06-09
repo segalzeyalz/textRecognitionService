69adc9f1cc0a0758996885fab26b358cb72097e5
segalzeyalz <segaleseo@gmail.com>
2019-06-09 08:33:13 +0300

    backend fixes - using other port, etc.

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -1,12 +1,8 @@
 const imageOcrService = require("../modules/imageOCR");
 module.exports = express => {
   const router = express.Router();
-  router.post("/api", (req, res) => {
-    console.log("url", req.body.url);
+  router.post("/image", (req, res) => {
     const { url } = req.body;
-    // const url =
-    //   `http://${req.params.url}` ||
-    //   "http://tesseract.projectnaptha.com/img/eng_bw.png";
     imageOcrService(req, res, url);
   });
 
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ let cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const routers = require("./routers")(express);
-const port = 3000;
+const port = 3001;
 const app = express();
 app.use(cors());
 app.use(bodyParser());
-app.use("/", routers);
+app.use("/api", routers);
 
 // launch our backend into a port
 app.listen(port, () => console.log(`LISTENING ON PORT ${port}`));
